c827bf21625e4cb46a5389d0c967e547a149004d
Oliver Pieper <pieper@viaregio.de>
2022-12-11 11:17:41 +0100

    Logging text compacted

--- sample_dummies.py
+++ sample_dummies.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 def clone_main():
-    logger.info("clone_main: Emulating the cloning of the main project repositories.")
+    logger.info("clone_main: Emulate cloning of main project repositories.")
     for i in range(2):
         logger.info(f"clone_main: clone main {i + 1}")
         time.sleep(3)
@@ -15,9 +15,7 @@ def clone_main():
 
 
 def clone_remaining():
-    logger.info(
-        "clone_remaining: Emulating the cloning of additional main project repositories."
-    )
+    logger.info("clone_remaining: Emulate cloning of additional project repositories.")
     for i in range(5):
         logger.info(f"clone_remaining: clone helper {i + 1}")
         time.sleep(i + 1)
