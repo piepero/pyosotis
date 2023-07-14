2eb5bb1d007b47e6cf6d3ed13d216efd7870b5f6
Oliver Pieper <pieper@viaregio.de>
2023-07-14 12:58:26 +0200

    Show description, show log
    - tk.Text changed to ScrolledText
    - display log messages in one ScrolledText widget
    - display description of selected task in new ScrolledText widget

--- src/pyosotis/gui.py
+++ src/pyosotis/gui.py
@@ -1,6 +1,10 @@
+import queue
 import tkinter as tk
 from collections import namedtuple
+from logging import Handler
+from queue import Queue
 from tkinter import Listbox, StringVar, ttk
+from tkinter.scrolledtext import ScrolledText
 
 from loguru import logger
 
@@ -16,6 +20,15 @@ close_requested = False
 def close_window():
     global close_requested
     close_requested = True
+
+
+class QueueHandler(Handler):
+    def __init__(self, queue):
+        Handler.__init__(self)
+        self.queue = queue
+
+    def emit(self, record):
+        self.queue.put(record)
 
 
 class PyosotisGui:
@@ -34,12 +47,17 @@ class PyosotisGui:
         tk_root.columnconfigure(0, weight=1)
 
         self.place_gui_components(self.mainframe)
+
+        self.log_queue: Queue = Queue()
+        self.log_queue_handler = QueueHandler(self.log_queue)
+        logger.add(self.log_queue_handler)
+
         self.update_gui()
 
     def place_gui_components(self, tk_root):
         def _place_task_block(label_text, button_text=None, button_command=None):
-            """Create a consecutive block of a text label, a listbox and the listbox choices
-            and return a TaskBlock with references to the new items."""
+            """Create a consecutive block of a text label, a listbox and the listbox
+            choices and return a TaskBlock with references to the new items."""
             lf = ttk.LabelFrame(tk_root, text=label_text)
 
             btn = None
@@ -64,6 +82,8 @@ class PyosotisGui:
             "Mark selected task as done",
             self.btn_due_done,
         )
+
+        self.tb_due.listbox.bind("<<ListboxSelect>>", self.due_task_selected)
         self.tb_running = _place_task_block("Currently running automatic tasks")
         self.tb_waiting = _place_task_block("Waiting tasks")
         self.tb_finished = _place_task_block("Finished tasks")
@@ -73,10 +93,21 @@ class PyosotisGui:
         self.tb_waiting.labelframe.grid(row=1, column=0, stick="nsew")
         self.tb_finished.labelframe.grid(row=1, column=1, stick="nsew")
 
-        lf = ttk.LabelFrame(tk_root, text="Info")
-        self.tx_messages = tk.Text(lf)
-        self.tx_messages.pack(side="top", fill="both")
+        lf = ttk.LabelFrame(tk_root, text="Task Description")
+        self.tx_description = ScrolledText(lf, wrap=tk.WORD)
+        self.tx_description.pack(side="top", fill="both")
         lf.grid(row=2, column=0, columnspan=2, stick="nsew")
+
+        lf = ttk.LabelFrame(tk_root, text="Log")
+        self.tx_messages = ScrolledText(lf, wrap=tk.NONE)
+        self.tx_messages.pack(side="top", fill="both")
+        lf.grid(row=3, column=0, columnspan=2, stick="nsew")
+
+        self.tx_messages.tag_config("INFO", foreground="black")
+        self.tx_messages.tag_config("DEBUG", foreground="gray")
+        self.tx_messages.tag_config("WARNING", foreground="orange")
+        self.tx_messages.tag_config("ERROR", foreground="red")
+        self.tx_messages.tag_config("CRITICAL", foreground="red", underline=True)
 
         (cols, rows) = tk_root.grid_size()
         for r in range(rows):
@@ -85,25 +116,48 @@ class PyosotisGui:
             tk_root.columnconfigure(c, weight=1)
 
     def update_gui(self) -> None:
+        """Update the GUI."""
+
+        # if requested by the global flag, close the program
         global close_requested
         if close_requested:
             self.tk_root.destroy()
             return
+
+        # show log messages from queue
+        while True:
+            try:
+                rec = self.log_queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                self.tx_messages.insert(
+                    tk.END, self.log_queue_handler.format(rec) + "\n", rec.levelname
+                )
+                self.tx_messages.yview(tk.END)
+
+        # update the status of all tasks
         self.runner.update_tasks()
-        self.tb_due.listbox_choices.set([t.title for t in self.runner.due_tasks])
-        self.tb_running.listbox_choices.set(
-            [t.title for t in self.runner.running_tasks]
-        )
-        self.tb_waiting.listbox_choices.set(
-            [t.title for t in self.runner.waiting_tasks]
-        )
-        self.tb_finished.listbox_choices.set(
-            [t.title for t in self.runner.finished_tasks]
-        )
-        self.tk_root.after(1000, self.update_gui)
+
+        # write the task titles to the appropriate listboxes
+        self.tb_due.listbox_choices.set(self.runner.due_tasks.titles)
+        self.tb_running.listbox_choices.set(self.runner.running_tasks.titles)
+        self.tb_waiting.listbox_choices.set(self.runner.waiting_tasks.titles)
+        self.tb_finished.listbox_choices.set(self.runner.finished_tasks.titles)
+
+        # schedule next GUI update
+        self.tk_root.after(500, self.update_gui)
 
     def btn_due_done(self, *args):
         for i in self.tb_due.listbox.curselection():
             self.runner.finish_due_task(i)
-            self.tx_messages.insert(tk.END, f"finished task {i}\n")
+            logger.info(f"task {i} manually marked as done")
         self.update_gui()
+
+    def due_task_selected(self, event):
+        selection = event.widget.curselection()
+        if not selection:
+            return
+        task = self.runner.due_tasks.tasks[selection[0]]
+        self.tx_description.delete("1.0", tk.END)
+        self.tx_description.insert(tk.END, task.description)
